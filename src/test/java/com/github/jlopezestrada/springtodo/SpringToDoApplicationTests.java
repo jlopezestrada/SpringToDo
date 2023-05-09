6be29bb9a5e3b8781264034f77247802c7f9aa53
José Ángel <jose.a.l.e@hotmail.com>
2023-05-09 10:14:59 +0200

    Initial Spring project

---
+++ src/test/java/com/github/jlopezestrada/springtodo/SpringToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jlopezestrada.springtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
